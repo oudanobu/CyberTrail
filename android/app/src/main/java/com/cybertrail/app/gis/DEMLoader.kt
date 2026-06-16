b7b707eb5534698f5b739aaec9f3b0e26f62a579
oudanobu <n16234637@gmail.com>
2026-06-16 21:23:15 +0800

    feat(gis): add IOException import to DEMLoader

--- android/app/src/main/java/com/cybertrail/app/gis/DEMLoader.kt
+++ android/app/src/main/java/com/cybertrail/app/gis/DEMLoader.kt
@@ -3,6 +3,7 @@ package com.cybertrail.app.gis
 import android.content.Context
 import android.util.Log
 import java.io.File
+import java.io.IOException
 import java.io.RandomAccessFile
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
